aa1eb99b70da855f175631cea5f75399da8bfeaf
Mr Code <omarafifi846@gmail.com>
2026-08-11 20:00:51 +0300

    ..

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,3 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Development ");
+﻿using DesignPattern.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Problem
+
+            // var obj01 = new ConfigurationManager();
+            // var obj02 = new ConfigurationManager();
+            //
+            // Console.WriteLine(obj01 == obj02); 
+
+            #endregion
+
+            #region Egar Singleton
+
+            //ConfigurationManager.ShowMessage();
+
+            //var obj01 = ConfigurationManager.Instance;
+
+
+            // OutPut
+            //Object Created
+            //Static Constructor Called
+            //Hello from ConfigurationManager
+
+            #endregion
+
+            #region Lazy Singleton
+
+            //   ConfigurationManager.ShowMessage();
+            //   Console.WriteLine("TESTing");
+            //   var obj01 = ConfigurationManager.Instance;
+            //   var obj02 = ConfigurationManager.Instance;
+            //   Console.WriteLine(obj01 == obj02);
+            // OutPut
+            //Hello from Configuration Manager Message
+            //TESTing
+            //Object Created
+            //True
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ DesignPattern/Singleton/ConfigurationManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+    internal class ConfigurationManager
+    {
+        #region Egar Singleton
+        //private static readonly ConfigurationManager _instance = new ConfigurationManager();
+
+        //static ConfigurationManager()
+        //{
+        //    Console.WriteLine("Static Constructor Called");
+        //}
+        //private ConfigurationManager()
+        //{
+        //    Console.WriteLine("Object Created ");
+        //}
+        //public static ConfigurationManager Instance => _instance;
+
+
+        //public static void ShowMessage()
+        //{
+        //    Console.WriteLine("Hello from ConfigurationManager");
+        //}
+        #endregion
+
+        #region Lazy Singleton
+
+        //private static  ConfigurationManager _instance;
+
+        //private ConfigurationManager()
+        //{
+        //    Console.WriteLine("Object Created ");
+        //}
+        //public static ConfigurationManager Instance
+        //{
+        //    get
+        //    {
+        //        if (_instance == null)
+        //             _instance = new ConfigurationManager();
+        //        return _instance;
+        //    }
+        //}
+        //public static void ShowMessage()
+        //{
+        //    Console.WriteLine("Hello from Configuration Manager Message");
+        //}
+
+        #endregion
+
+        #region Lock Thread
+
+        //private static ConfigurationManager _instance;
+        //private static readonly object _lock = new object();
+
+        //private ConfigurationManager()
+        //{
+        //    Console.WriteLine("Object Created");
+        //}
+
+        //public static ConfigurationManager Instance
+        //{
+        //    get
+        //    {
+        //        lock (_lock)
+        //        {
+        //            if (_instance == null)
+        //                _instance = new ConfigurationManager();
+
+        //            return _instance;
+        //        }
+        //    }
+        //}
+        #endregion
+
+        #region Lazy<T>
+
+        private static readonly Lazy<ConfigurationManager> _instance =
+        new Lazy<ConfigurationManager>(() => new ConfigurationManager());
+        private ConfigurationManager()
+        {
+            Console.WriteLine("Object Created");
+        }
+
+        public static ConfigurationManager Instance => _instance.Value;
+
+        public static void ShowMessage()
+        {
+            Console.WriteLine("Hello from Configuration Manager Message");
+        }
+
+        #endregion
+
+    }
+}
