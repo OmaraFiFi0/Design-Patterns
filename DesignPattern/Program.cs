ceda10aceb15c8f6f3ff467c498c2439134da200
Mr Code <omarafifi846@gmail.com>
2026-08-12 05:37:02 +0300

    ..

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Development ");
+﻿
+namespace DesignPattern
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+
+        }
+    }
+}
